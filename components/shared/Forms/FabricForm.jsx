45518930911e5679230f3649d8814ba9b864ba21
Atif Hameed <atifhameed11312@gmail.com>
2024-09-30 22:50:17 +0500

    fix changes

--- components/shared/Forms/FabricForm.jsx
+++ components/shared/Forms/FabricForm.jsx
@@ -11,17 +11,26 @@ const FabricForm = ({ data, selectedFabric, setSelectedFabric, file, setFile, te
         setSelectedFabric(name); // Only one can be selected at a time
     };
 
+    const handleBoxClick = (name) => {
+        setSelectedFabric(name); 
+    };
+
     return (
         <div className='py-9'>
             <Heading>Choose your fabric</Heading>
             <div className='w-full grid lg:grid-cols-3 sm:grid-cols-2 grid-cols-1 gap-3'>
                 {data.map((e, i) => (
-                    <div key={i} className='bg-lightBackground  sm:h-[132px] h-24 p-5 flex items-start'>
+                    <div
+                        key={i}
+                        className='bg-lightBackground sm:h-[132px] h-24 p-5 flex items-start cursor-pointer'
+                        onClick={() => handleBoxClick(e.name)}
+                    >
                         <div className='flex items-center gap-3 justify-between w-full'>
                             <div className='flex items-center gap-2'>
                                 <CustomCheckbox
                                     name={e.name}
                                     checked={selectedFabric === e.name} // Only one can be checked
+                                    onClick={(e) => e.stopPropagation()} // Prevent triggering box click on checkbox click
                                     onChange={() => handleCheckboxChange(e.name)}
                                 />
                                 <p className='sm:text-xl text-lg font-semibold text-dark'>{e.label}</p>
@@ -34,8 +43,6 @@ const FabricForm = ({ data, selectedFabric, setSelectedFabric, file, setFile, te
                                     :
                                     <HiQuestionMarkCircle className='text-lightBlue sm:text-2xl text-xl w-8' />
                             }
-
-
                         </div>
                     </div>
                 ))}
--- components/shared/Forms/PackagingForm.jsx
+++ components/shared/Forms/PackagingForm.jsx
@@ -4,14 +4,29 @@ import React, { useState } from 'react';
 import Heading from '../Heading';
 import CustomDataUpload from '../CustomDataUpload';
 import Image from 'next/image';
-import packaging from '../../../assets/images/ploybag.png';
+import packaging from '@/assets/images/ploybag.png';
+import polybagEmpty from '@/assets/images/polybagEmpty.png'
 import CustomRadioButton from '../CustomRadioButton';
+import { RiDeleteBin6Line } from 'react-icons/ri';
+import { IoCloudUploadOutline } from 'react-icons/io5';
 
 const PackagingForm = () => {
     const [selectedOption, setSelectedOption] = useState('polybag');
+    const [ownLogo, setOwnLogo] = useState(null);
 
     const handleOptionChange = (value) => {
         setSelectedOption(value);
+    };
+
+    const handleOwnLogoFileChange = (e) => {
+        const file = e.target.files[0];
+        if (file) {
+            setOwnLogo(file);
+        }
+    };
+
+    const onOwnLogoFileRemove = () => {
+        setOwnLogo(null);
     };
 
     return (
@@ -26,7 +41,7 @@ const PackagingForm = () => {
                             <p className='py-7 text-gray-700 text-lg'>
                                 Do you want us to pack every piece in an unbranded polybag, made from recycled plastic, or have them unpackaged?
                             </p>
-                            <div className="flex gap-7 pt-10">
+                            <div className="flex gap-7 flex-wrap pt-10">
                                 <div>
                                     <CustomRadioButton
                                         label={'Neutral Polybag'}
@@ -48,6 +63,15 @@ const PackagingForm = () => {
                                         isChecked={selectedOption === 'unpackaged'} // Check if unpackaged is selected
                                     />
                                 </div>
+                                <div>
+                                    <CustomRadioButton
+                                        label={'Own logo'}
+                                        name={'radio'}
+                                        value={'ownlogo'}
+                                        onChange={handleOptionChange}
+                                        isChecked={selectedOption === 'ownlogo'} // Check if ownlogo is selected
+                                    />
+                                </div>
                             </div>
                         </div>
                         <div className="py-10">
@@ -55,7 +79,56 @@ const PackagingForm = () => {
                         </div>
                     </div>
                     <div className="md:col-span-7">
-                        <Image src={packaging} alt='packaging' />
+                        {
+                            selectedOption === 'polybag' ? (
+                                <Image src={packaging} alt='packaging' />
+                            ) : selectedOption === 'ownlogo' ? (
+                                <div className='relative w-fit'>
+                                    <Image src={polybagEmpty} alt='packaging' />
+
+                                    <div className='z-20 absolute top-12 left-1/2 -translate-x-1/2 cursor-pointer py-3 flex flex-col justify-start gap-2 items-center'>
+                                        {/* Display uploaded file type or preview */}
+                                        <div>
+                                            {ownLogo ? (
+                                                <div className='w-12 h-12 bg-lightBlueText flex items-center justify-center rounded-lg'>
+                                                    <span className='text-blue-500 font-bold'>
+                                                        {ownLogo.type.split('/')[1].toUpperCase()}
+                                                    </span>
+                                                </div>
+                                            ) : (
+                                                <p></p>
+                                            )}
+                                        </div>
+                                    </div>
+
+                                    <div className='absolute top-[20%] left-1/2 -translate-x-1/2 z-40'>
+                                        {/* Upload button if no file is uploaded */}
+                                        {!ownLogo && (
+                                            <div className='flex flex-col items-center gap-3'>
+                                            <div className='bg-lightBlueText p-2 z-40 rounded-full hover:bg-lightBlue'>
+                                                <IoCloudUploadOutline className='text-2xl cursor-pointer text-white' />
+                                                <input
+                                                    type='file'
+                                                    className='absolute top-0 left-0 w-full h-full opacity-0 cursor-pointer z-10'
+                                                    onChange={handleOwnLogoFileChange} // Handle logo file upload
+                                                />
+                                            </div>
+                                            <p>Upload Own logo</p>
+                                            </div>
+                                        )}
+                                        {/* Delete button if file is uploaded */}
+                                        {ownLogo && (
+                                            <div
+                                                onClick={onOwnLogoFileRemove}
+                                                className='bg-white cursor-pointer z-40 p-3 relative rounded-full hover:bg-[#f3f6f9] mt-4'
+                                            >
+                                                <RiDeleteBin6Line className='text-lightBlue text-2xl' />
+                                            </div>
+                                        )}
+                                    </div>
+                                </div>
+                            ) : null
+                        }
                     </div>
                 </div>
             </div>
