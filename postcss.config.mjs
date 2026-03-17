b0af1f25e0e6fc300709076565126363dbf2a33c
Atif Hameed <atifhameed11312@gmail.com>
2026-03-17 23:14:33 +0500

    Remove PostCSS configuration and add placeholder text

--- postcss.config.mjs
+++ postcss.config.mjs
@@ -1,9 +1,1 @@
-import { createRequire } from 'module';
-const require = createRequire(import.meta.url);
-/** @type {import('postcss-load-config').Config} */
-const config = {
-  plugins: {
-    tailwindcss: {},
-  },
-};
-export default config;
+helo
